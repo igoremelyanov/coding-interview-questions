3fede8c73381e83d05c88a952e25cb34e54b7614
igor.emelyanov <igor.emelyanov@afusion.com>
2015-07-28 15:26:05 -0700

    Add Random String Generator

--- Get Rundom.cs
+++ Get Rundom.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using NUnit.Framework;
@@ -11,8 +12,10 @@ namespace DevTarining
     public class Get_Rundom
     {
         int _randomNumber;
-        
-        public class DataGenerator
+        public string _randomString;
+        public const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+        public class DataGenerator_RundomNumber
         {
             Random _rundom = new Random();
             public int GetRandomNumber(int min, int max)
@@ -21,19 +24,39 @@ namespace DevTarining
             }
         }
 
-        [Test]
+        public class DataGenerator_RundomString
+        {
+            Random _rundom = new Random();
+            public string GetRandomString(int size, string charsToUse)
+            {
+                var buffer = new char[size];
+
+                for(int i = 0; i < size; i++)
+                {
+                     buffer[i] = charsToUse[_rundom.Next(charsToUse.Length)];
+                }
+                return new string(buffer);
+            }
+        }
+
+        [Test, Repeat(4)]
         public void Get_Rundom_Number()
         {
-            DataGenerator dataGenerator = new DataGenerator();
+            DataGenerator_RundomNumber number_dataGenerator = new DataGenerator_RundomNumber();
            
-            _randomNumber =dataGenerator. GetRandomNumber(1,100);
+            _randomNumber =number_dataGenerator.GetRandomNumber(1,100);
 
             Console.WriteLine("{0}", _randomNumber);
         }
 
-        [Test]
+        [Test, Repeat(4)]
         public void Get_Rundom_String()
         {
+            DataGenerator_RundomString str_dataGenerator = new DataGenerator_RundomString();
+
+            _randomString = str_dataGenerator.GetRandomString(20, Chars);
+
+            Console.WriteLine("{0}", _randomString);
             
         }
     }
